913019fa5b7347c4973ff06e6f915a2cf597de53
payl0adxff <105820294+payl0adxff@users.noreply.github.com>
2022-05-18 17:12:06 -0300

    change init

---
+++ connect-astro.py
@@ -0,0 +1,10 @@
+import requests
+
+if __name__ == "__main__":
+    timeout = 10
+    url = "https://astrobox.hotmart.com/"
+    header_data = {'Content-Type': 'application/x-www-form-urlencoded'}
+    header_data = {}
+    payload = "username=joao.snow@hotmart.com"
+    r = requests.request("POST", url, data=payload, headers=header_data, timeout=timeout)
+    print (r.request.body)
